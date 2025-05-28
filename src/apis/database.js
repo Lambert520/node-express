0907ec59aab69dcfdefd3662b7567b34bc0f5784
liubao <2423665044@qq.com>
2025-05-28 14:11:07 +0800

    feat: 增加数据库增删操作

--- src/apis/database.js
+++ src/apis/database.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../utils/db');
 
-router.get('/info', (req, res) => {
+router.get('/', (req, res) => {
   db.query('SELECT * FROM student', (error, results) => {
     if (error) {
       return res.status(500).json({ error: '数据库查询失败' });
@@ -11,4 +11,23 @@ router.get('/info', (req, res) => {
   });
 });
 
+router.get('/delete/:id', (req, res) => {
+  const { id } = req.params;
+  db.query('DELETE FROM student WHERE id = ?', [id], (error, results) => {
+    if (error) {
+      return res.status(500).json({ error: '数据库删除失败' });
+    }
+    res.json({ message: '学生信息删除成功' });
+  });
+});
+
+router.post('/insert', (req, res) => {
+  const { id, name, age } = req.body;
+  db.query('INSERT INTO student (id, name, age) VALUES (?, ?, ?)', [id, name, age], (error, results) => {
+    if (error) {
+      return res.status(500).json({ error: '数据库插入失败' });
+    }
+    res.status(200).json({ message: '学生信息添加成功', id: results.insertId });
+  });
+});
 module.exports = router;
